8b8ab65d31c9332dad36f6bb2c4acb3d7943e20e
Ricardo Martin Marcucci <rmarku@gmail.com>
2017-03-14 21:30:33 -0300

    Prueba 2 Travis.ci

--- .deploy.sh
+++ .deploy.sh
@@ -4,8 +4,8 @@ npm install
 set -e # Exit with nonzero exit code if anything fails
 
 # Now let's go have some fun with the cloned repo
-git config user.name "Travis CI"
-git config user.email "$(git --no-pager show -s --format='%ae' HEAD)"
+git config --global user.name "Travis CI"
+git config --global user.email "$(git --no-pager show -s --format='%ae' HEAD)"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 openssl aes-256-cbc -K $encrypted_8b5ed4773e26_key -iv $encrypted_8b5ed4773e26_iv -in deploy_key.enc -out deploy_key -d
